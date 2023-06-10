d1941a85345f7548f7c9d9911985c7c3d917c497
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-10 12:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function SizeInfo()
+{
+var x = document.documentElement.clientWidth;
+var y = document.documentElement.clientHeight;
+        
+// put the result into a h1 tag
+ document.getElementById('wh').innerHTML = "<h1>Width: " + x + "  Height: " + y + "</h1>";
+}
